f8a601b6146d1eb0410c5a9bd9dedceae91bf5c5
Kevin A. Wilson <kwilson@bluecord.org>
2017-10-05 11:07:31 -0400

    Additional optimizations

--- Unused code.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-    <?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-        <?php endif; ?>
-
-            <!-- Before title-bar-left -->
-            <div class="title-bar-right">
-                <button class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
-            </div>
